a06c55e23caed836ea74ccec9ae70cc428502dcb
Phue-Pwint-Phway <scm.phuepwintphway@gmail.com>
2021-03-03 13:14:25 +0630

    first commit

--- database/migrations/2021_03_03_062801_create_posts_table.php
+++ database/migrations/2021_03_03_062801_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->char('name')->unique();
-            $table->char('email')->unique();
-            $table->text('password');
-            $table->char('profile');
-            $table->char('type');
-            $table->char('phone')->nullable();
-            $table->char('address')->nullable();
-            $table->date('dob')->nullable();
+            $table->char('title');
+            $table->char('description');
+            $table->char('status');
 
             $table->bigInteger('create_user_id')->unsigned();
             // $table->foreign('create_user_id')->references('id')->on('users');
@@ -31,9 +26,9 @@ class CreateUsersTable extends Migration
             // $table->foreign('updated_user_id')->refernces('id')->on('users');
 
             $table->integer('deleted_user_id')->nullable();
-            $table->dateTime('user_created_at')->nullable();
-            $table->dateTime('user_updated_at')->nullable();
-            $table->dateTime('user_deleted_at')->nullable();
+            $table->dateTime('post_created_at')->nullable();
+            $table->dateTime('post_updated_at')->nullable();
+            $table->dateTime('post_deleted_at')->nullable();
             $table->timestamps();
         });
     }
@@ -45,6 +40,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
